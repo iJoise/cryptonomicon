b34fabbcf68a41640dd24f9a1c18972414e61cc7
Кирилл Кондратьев <kondratevk.1989@mail.ru>
2022-05-03 08:16:44 +0700

    Подключены вебсокеты

--- src/api/api.js
+++ src/api/api.js
@@ -1,40 +1,86 @@
 const BASE_URL = process.env.VUE_APP_BASE_URL;
 const API_KEY = process.env.VUE_APP_CRYPTO_KEY;
+const SOCKET_URL = process.env.VUE_APP_SOCKET_URL;
+
+const AGGREGATE_INDEX = "5";
 
 const tickersHandlers = new Map();
+const socket = new WebSocket(`${SOCKET_URL}/v2?api_key=${API_KEY}`);
 
-const loadTickers = () => {
-  if (tickersHandlers.size === 0) {
+socket.addEventListener("message", e => {
+  const { TYPE: type, FROMSYMBOL: currency, PRICE: newPrice } = JSON.parse(e.data);
+  if (type !== AGGREGATE_INDEX || newPrice === undefined) {
     return;
   }
 
-  fetch(
-    `${BASE_URL}pricemulti?fsyms=${[...tickersHandlers.keys()].join(
-      ","
-    )}&tsyms=USD&api_key=${API_KEY}`
-  )
-    .then(res => res.json())
-    .then(rawData => {
-      const updatedPrices = Object.fromEntries(
-        Object.entries(rawData).map(([key, value]) => [key, value.USD])
-      );
+  const handlers = tickersHandlers.get(currency) ?? [];
+  handlers.forEach(fn => fn(newPrice));
+});
 
-      Object.entries(updatedPrices).forEach(([currency, newPrice]) => {
-        const handlers = tickersHandlers.get(currency) ?? [];
-        handlers.forEach(fn => fn(newPrice));
-      });
-    });
-};
+function sendToWebSocket(message) {
+  const stringifiedMessage = JSON.stringify(message);
+  if (socket.readyState === WebSocket.OPEN) {
+    socket.send(stringifiedMessage);
+    return;
+  }
+
+  socket.addEventListener(
+    "open",
+    () => {
+      socket.send(stringifiedMessage);
+    },
+    { once: true }
+  );
+}
+
+function subscribeToTickerOnWs(ticker) {
+  sendToWebSocket({
+    action: "SubAdd",
+    subs: [`5~CCCAGG~${ticker}~USD`]
+  });
+}
+
+function unsubscribeToTickerOnWs(ticker) {
+  sendToWebSocket({
+    action: "SubRemove",
+    subs: [`5~CCCAGG~${ticker}~USD`]
+  });
+}
 
 export const subscribeToTickers = (ticker, cb) => {
   const subscribers = tickersHandlers.get(ticker) || [];
   tickersHandlers.set(ticker, [...subscribers, cb]);
+  subscribeToTickerOnWs(ticker);
 };
 
 export const unsubscribeToTicker = ticker => {
   tickersHandlers.delete(ticker);
+  unsubscribeToTickerOnWs(ticker);
 };
 
-setInterval(loadTickers, 5000);
+// setInterval(loadTickers, 5000);
 
 window.tickers = tickersHandlers;
+
+// const loadTickers = () => {
+//   if (tickersHandlers.size === 0) {
+//     return;
+//   }
+
+//   fetch(
+//     `${BASE_URL}pricemulti?fsyms=${[...tickersHandlers.keys()].join(
+//       ","
+//     )}&tsyms=USD&api_key=${API_KEY}`
+//   )
+//     .then(res => res.json())
+//     .then(rawData => {
+//       const updatedPrices = Object.fromEntries(
+//         Object.entries(rawData).map(([key, value]) => [key, value.USD])
+//       );
+
+//       Object.entries(updatedPrices).forEach(([currency, newPrice]) => {
+//         const handlers = tickersHandlers.get(currency) ?? [];
+//         handlers.forEach(fn => fn(newPrice));
+//       });
+//     });
+// };
